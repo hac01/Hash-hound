04720e1bdf95660bc6dedfa54348b53864bd5057
hac <70646122+hac01@users.noreply.github.com>
2023-05-04 22:33:50 +0530

    Update hash.py
    Added help menu and now it also shows hashcat mode

--- hash.py
+++ hash.py
@@ -1,27 +1,38 @@
 import sys
 from termcolor import colored
+import pandas
 
 def main():
+    # Check for help menu
+    if len(sys.argv) > 1 and (sys.argv[1] == '-h' or sys.argv[1] == '--help'):
+        print('Usage: python script.py <hash_value>')
+        print(''' 
+        For example :- hash-hound 8d6e34f987851aa599257d3831a1af040886842f
+        Output:- Algorithm used: SHA1:100 
+        Here SHA1 is the hashing algorithm and 100 is Hashcat mode for ex: "-m 100"
+        ''')
+        return
+
     hash_value = sys.argv[1]
 
     hash_lengths_to_algorithms = {
-        16: "MD4",
-        20: "SHA1",
-        24: "CRC24",
-        28: "SHA224",
-        32: "MD5",
-        40: "SHA1",
-        48: "SHA384",
-        56: "SHA224",
-        64: ["SHA256", "HMAC-SHA256", "NTLM", "WPA"],
-        96: ["SHA384", "HMAC-SHA384"],
-        128: ["SHA512", "HMAC-SHA512", "FNV-1a"],
-        160: "SHA1",
-        256: ["SHA256", "FNV-1a", "WPA2"],
-        320: "FNV-1a",
-        384: "SHA384",
-        512: "SHA512",
-    }
+            16: "MD4:0",
+            20: "SHA1:100",
+            24: "CRC24",
+            28: "SHA224:5300",
+            32: "MD5:0",
+            40: "SHA1:100",
+            48: "SHA384:14600",
+            56: "SHA224:5300",
+            64: ["SHA256:1400", "HMAC-SHA256:7500", "NTLM", "WPA"],
+            96: ["SHA384:14700", "HMAC-SHA384:14600"],
+            128: ["SHA512:1700", "HMAC-SHA512:17500", "FNV-1a"],
+            160: "SHA1:100",
+            256: ["SHA256:1400", "FNV-1a", "WPA2"],
+            320: "FNV-1a",
+            384: "SHA384:14600",
+            512: "SHA512:1700",
+        }
 
     # Identify the algorithm used for the hash value
     algorithm = hash_lengths_to_algorithms.get(len(hash_value), "Unknown algorithm")
