4bb2f637db03ce2df553998163279c245e1dde10
hac <70646122+hac01@users.noreply.github.com>
2023-05-04 18:08:30 +0530

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,32 +1,36 @@
 import sys
 from termcolor import colored
 
-hash_value = sys.argv[1]
+def main():
+    hash_value = sys.argv[1]
 
-hash_lengths_to_algorithms = {
-    16: "MD4",
-    20: "SHA1",
-    24: "CRC24",
-    28: "SHA224",
-    32: "MD5",
-    40: "SHA1",
-    48: "SHA384",
-    56: "SHA224",
-    64: ["SHA256", "HMAC-SHA256", "NTLM", "WPA"],
-    96: ["SHA384", "HMAC-SHA384"],
-    128: ["SHA512", "HMAC-SHA512", "FNV-1a"],
-    160: "SHA1",
-    256: ["SHA256", "FNV-1a", "WPA2"],
-    320: "FNV-1a",
-    384: "SHA384",
-    512: "SHA512",
-}
+    hash_lengths_to_algorithms = {
+        16: "MD4",
+        20: "SHA1",
+        24: "CRC24",
+        28: "SHA224",
+        32: "MD5",
+        40: "SHA1",
+        48: "SHA384",
+        56: "SHA224",
+        64: ["SHA256", "HMAC-SHA256", "NTLM", "WPA"],
+        96: ["SHA384", "HMAC-SHA384"],
+        128: ["SHA512", "HMAC-SHA512", "FNV-1a"],
+        160: "SHA1",
+        256: ["SHA256", "FNV-1a", "WPA2"],
+        320: "FNV-1a",
+        384: "SHA384",
+        512: "SHA512",
+    }
 
-# Identify the algorithm used for the hash value
-algorithm = hash_lengths_to_algorithms.get(len(hash_value), "Unknown algorithm")
+    # Identify the algorithm used for the hash value
+    algorithm = hash_lengths_to_algorithms.get(len(hash_value), "Unknown algorithm")
 
-if isinstance(algorithm, list):
-    algorithms = ", ".join(algorithm)
-    print(colored(f"Matching algorithms: {algorithms}", "red"))
-else:
-    print(colored(f"Algorithm used: {algorithm}", "red"))
+    if isinstance(algorithm, list):
+        algorithms = ", ".join(algorithm)
+        print(colored(f"Matching algorithms: {algorithms}", "red"))
+    else:
+        print(colored(f"Algorithm used: {algorithm}", "red"))
+
+if __name__ == "__main__":
+    main()
